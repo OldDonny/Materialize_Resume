dbc33ffa79bbb4a9dbb32228a1ef9340c6d883a5
olddonny <reedtrekks@gmail.com>
2017-09-21 18:25:06 -0500

    intial style and parallax scroll

---
+++ scrips.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function(){
+            $(".button-collapse").sideNav();
+            $('.carousel.carousel-slider').carousel({fullWidth: true});
+            $('.parallax').parallax();
+        });
+
