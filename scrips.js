ba4e679d8b4cfff7e7ebfb87744ed85bbdc18354
olddonny <reedtrekks@gmail.com>
2017-09-22 17:41:17 -0500

    main-format

--- scrips.js
+++ scrips.js
@@ -3,5 +3,8 @@ $(document).ready(function(){
             $(".button-collapse").sideNav();
             $('.carousel.carousel-slider').carousel({fullWidth: true});
             $('.parallax').parallax();
-        });
+            $('.carousel.carousel-slider').on('click',function(){
+                $('.carousel.carousel-slider').carousel('next');
+            });
+});
 
